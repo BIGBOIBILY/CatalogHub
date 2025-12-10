ff864b8051db4424aac20113894361830976b300
Alyson Souza Carregosa <alysonsouza36@gmail.com>
2025-12-10 16:33:36 -0300

    ✨feat: define core entities & repository interfaces
    - Added Product and Category domain models
    - Defined IProductRepository and ICategoryRepository interfaces
    - Configured Domain project

---
+++ CatalogHub.Domain/Interfaces/Repository/ICategoryRepository.cs
@@ -0,0 +1,12 @@
+﻿using CatalogHub.Domain.Models;
+
+namespace CatalogHub.Domain.Interfaces.Repository;
+
+public interface ICategoryRepository
+{
+    Task<Category?> CreateAsync(Category category);
+    Task<Category?> UpdateAsync(Category category);
+    Task<Category?> DeleteAsync(Category category);
+    Task<Category?> GetByIdAsync(Guid id);
+    Task<(List<Category> categories, int totalCount)> GetAllAsync(int pageNumber, int pageSize);
+}
---
+++ CatalogHub.Domain/Interfaces/Repository/IProductRepository.cs
@@ -0,0 +1,13 @@
+﻿using CatalogHub.Domain.Models;
+
+namespace CatalogHub.Domain.Interfaces.Repository;
+
+public interface IProductRepository
+{
+    Task<Product?> CreateAsync(Product product);
+    Task<Product?> UpdateAsync(Product product);
+    Task<Product?> DeleteAsync(Product product);
+    Task<Product?> GetByIdAsync(Guid id);
+    Task<(List<Product> products, int totalCount)> GetAllAsync(int pageNumber, int pageSize);
+    Task<List<Product>?> GetByFiltersAsync(Guid? categoryId, decimal? minPrice, decimal? maxPrice, bool? isActive);
+}
